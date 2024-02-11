473de99c90a03ab073dec9467d7dc18fca883a67
john-ngugi <johnngugi0407@gmail.com>
2024-02-11 17:02:43 +0300

    park update

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ map.on('click', function(e) {
                         var long = result.geometry.location.lng;
                         var customIcon = L.icon({
                             iconUrl: result.icon,    
-                            iconSize:     [38, 45], // size of the icon
+                            iconSize:     [30, 35], // size of the icon
                             iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
                             popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
                         });
