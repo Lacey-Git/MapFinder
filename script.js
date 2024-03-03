013b9ee0914029ad6cc5ef8fe73cbb05b38bebf9
john-ngugi <johnngugi0407@gmail.com>
2024-03-03 11:42:17 +0300

    layers update

--- script.js
+++ script.js
@@ -20,6 +20,36 @@ var result ;
 var query_location ;
 
 
+// Define maps
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    minZoom: 2,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
+
+
+var St = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
+
+var Th= L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	apikey: '10f74536e8ee47b29dc4ac6137a0b533',
+	maxZoom: 22
+});
+
+
+var Esri = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+
+
+
 console.log(searchbtn)
  // Creates a red marker with the coffee icon
  var redMarker = L.AwesomeMarkers.icon({
@@ -127,6 +157,23 @@ button.addEventListener('click', () => {
 
 // Initialize Leaflet map
 var map = L.map('map').setView([lat, long], 6.1);
+
+var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
+
+var basemaps = {
+    'carto': CartoDB_Voyager,
+    'Stamen_Lite': St,
+    'osm': osm,
+    'Thunder Forest': Th,
+    'Esri': Esri,
+}
+
+L.control.layers(basemaps).addTo(map)
+L.control.scale().addTo(map);
 
 // Function to round a number to specified digits
 function roundTo(n, digits) {
@@ -302,9 +349,4 @@ map.on('click', function(e) {
      
 
 
-// Add OpenStreetMap tile layer to the map
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    minZoom: 2,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map)
+
